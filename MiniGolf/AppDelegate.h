fa7e422960a1f6fa42cd4a0f32f491776ee2ba16
Bret Williams <bret@brets-mbp.tx.rr.com>
2017-12-18 16:58:05 -0600

    Initial Commit

---
+++ MiniGolf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiniGolf
+//
+//  Created by Bret Williams on 12/18/17.
+//  Copyright © 2017 Bret Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiniGolf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiniGolf
+//
+//  Created by Bret Williams on 12/18/17.
+//  Copyright © 2017 Bret Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
